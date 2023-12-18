e09982c89cde6171f1efb14f6c39ee71e8766755
Viacheslav Loginov <barthez.slavik@gmail.com>
2023-12-18 15:40:03 +0200

    Middlewars

--- main.go
+++ main.go
@@ -18,11 +18,24 @@ func main() {
 		return
 	}
 
+	// Parse middleware configurations from the DSL file
+	middlewareConfigs, err := parseMiddlewareConfig("app/Middlewares.aur")
+	if err != nil {
+		fmt.Println("Error parsing middleware configuration:", err)
+		return
+	}
+
 	for _, route := range routeConfigs {
-		fmt.Printf("Adding route %s %s\n", route.Method, route.Path)
-		http.HandleFunc(route.Path, func(w http.ResponseWriter, r *http.Request) {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			// This is a simplified handler. You might want to add more complex handling here.
 			fmt.Fprint(w, route.Response)
-		})
+		}
+
+		// Wrap the handler with middleware
+		handler = applyMiddleware(handler, middlewareConfigs)
+
+		fmt.Printf("Adding route %s %s\n", route.Method, route.Path)
+		http.HandleFunc(route.Path, handler)
 	}
 
 	fmt.Printf("Starting server at http://%s:%d\n", serverConfig.Host, serverConfig.Port)
---
+++ middleware.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// MiddlewareConfig represents the configuration of a middleware
+type MiddlewareConfig struct {
+	Name    string
+	Action  string
+	Options map[string]string
+}
+
+// parseMiddlewareConfig parses the middleware configuration from a DSL file
+func parseMiddlewareConfig(filePath string) ([]MiddlewareConfig, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var middlewares []MiddlewareConfig
+	scanner := bufio.NewScanner(file)
+
+	var currentMiddleware *MiddlewareConfig
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+
+		if strings.HasPrefix(line, "Middleware") {
+			parts := strings.Fields(line)
+			if len(parts) < 2 {
+				fmt.Println("Warning: skipping middleware due to insufficient arguments")
+				continue
+			}
+			currentMiddleware = &MiddlewareConfig{
+				Name:    parts[1],
+				Options: make(map[string]string),
+			}
+		} else if strings.HasPrefix(line, "Action:") {
+			if currentMiddleware != nil {
+				action := strings.TrimPrefix(line, "Action:")
+				currentMiddleware.Action = strings.TrimSpace(action)
+			}
+		} else {
+			keyVal := strings.SplitN(line, ":", 2)
+			if len(keyVal) == 2 && currentMiddleware != nil {
+				key := strings.TrimSpace(keyVal[0])
+				value := strings.TrimSpace(keyVal[1])
+				currentMiddleware.Options[key] = value
+			}
+		}
+
+		if line == "}" && currentMiddleware != nil {
+			middlewares = append(middlewares, *currentMiddleware)
+			currentMiddleware = nil
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return middlewares, nil
+}
+
+// applyMiddleware wraps an HTTP handler with the specified middlewares
+func applyMiddleware(handler http.HandlerFunc, middlewares []MiddlewareConfig) http.HandlerFunc {
+	for _, mw := range middlewares {
+		switch mw.Action {
+		case "LogRequest":
+			handler = logRequestMiddleware(handler)
+		case "Authenticate":
+			handler = authenticateMiddleware(handler, mw.Options["Key"])
+		}
+	}
+	return handler
+}
+
+// logRequestMiddleware is a middleware that logs each request
+func logRequestMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Request received: %s %s\n", r.Method, r.URL.Path)
+		next(w, r)
+	}
+}
+
+// authenticateMiddleware is a middleware that handles authentication
+func authenticateMiddleware(next http.HandlerFunc, key string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Authentication logic here...
+		// For example, check some header against the 'key'
+		authHeader := r.Header.Get("Authorization")
+		if authHeader != key {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		next(w, r)
+	}
+}
